b66bc1e110710b251ee70110cb4035e28720fde6
maximilian.schubert@telekom.de <maximilian.schubert@telekom.de>
2023-11-16 15:05:45 +0100

    feat: rm interval validation due to blocking request

--- pkg/config/validate.go
+++ pkg/config/validate.go
@@ -16,13 +16,6 @@ func (c *Config) Validate() error {
 		log.Error("The loaderHttpUrl is not set")
 	}
 
-	if c.Loader.http.timeout > c.Loader.Interval {
-		ok = false
-		log.Error("The loader http timeout must be smaller than the loader interval",
-			"loaderHttpTimeout", c.Loader.http.timeout,
-			"loaderInterval", c.Loader.Interval)
-	}
-
 	if c.Loader.http.retryCfg.Count < 0 || c.Loader.http.retryCfg.Count >= 5 {
 		ok = false
 		log.Error("The amount of loader http retries should be above 0 and below 6",
