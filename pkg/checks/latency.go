5184c9fb39d3bd282c7fd11163632a6ea8c136da
Niklas Treml <niklas.treml@telekom.de>
2023-11-29 17:21:43 +0100

    feat: only collect total duration

--- pkg/checks/latency.go
+++ pkg/checks/latency.go
@@ -2,9 +2,7 @@ package checks
 
 import (
 	"context"
-	"crypto/tls"
 	"net/http"
-	"net/http/httptrace"
 	"sync"
 	"time"
 
@@ -38,56 +36,18 @@ type Latency struct {
 type LatencyConfig struct {
 	Targets  []string
 	Interval time.Duration
-}
-
-type LatencyResultDTO struct {
-	Code  int
-	Error *string
-	DNS   time.Duration
-	TLS   time.Duration
-	Dial  time.Duration
+	Timeout  time.Duration
 }
 
 type LatencyResult struct {
-	Code  int
-	Error *string
-	DNS   Metric
-	TLS   Metric
-	Dial  Metric
-}
-
-func (s *LatencyResult) ToDTO() LatencyResultDTO {
-	return LatencyResultDTO{
-		Code:  s.Code,
-		Error: s.Error,
-		DNS:   s.DNS.Duration(),
-		TLS:   s.TLS.Duration(),
-		Dial:  s.Dial.Duration(),
-	}
-}
-
-type Metric struct {
-	Start time.Time
-	End   time.Time
-}
-
-func (m Metric) Duration() time.Duration {
-	return m.End.Sub(m.Start)
-}
-
-func WithLatency(ctx context.Context, l *LatencyResult) context.Context {
-	return httptrace.WithClientTrace(ctx, &httptrace.ClientTrace{
-		DNSStart:          func(di httptrace.DNSStartInfo) { l.DNS.Start = time.Now() },
-		DNSDone:           func(di httptrace.DNSDoneInfo) { l.DNS.End = time.Now() },
-		TLSHandshakeStart: func() { l.TLS.Start = time.Now() },
-		TLSHandshakeDone:  func(cs tls.ConnectionState, err error) { l.TLS.End = time.Now() },
-		ConnectStart:      func(network, addr string) { l.Dial.Start = time.Now() },
-		ConnectDone:       func(network, addr string, err error) { l.Dial.End = time.Now() },
-	})
+	Code  int     `json:"code"`
+	Error *string `json:"error"`
+	Total int64   `json:"total"`
 }
 
 func (l *Latency) Run(ctx context.Context) error {
 	log := logger.FromContext(ctx).WithGroup("Latency")
+	log.Info(l.cfg.Interval.String())
 	for {
 		select {
 		case <-ctx.Done():
@@ -133,6 +93,8 @@ func (l *Latency) SetConfig(ctx context.Context, config any) error {
 	if err != nil {
 		return ErrInvalidConfig
 	}
+	asSecond := time.Second * c.Interval
+	c.Interval = asSecond
 	l.mu.Lock()
 	defer l.mu.Unlock()
 	l.cfg = c
@@ -145,49 +107,63 @@ func (l *Latency) Schema() (*openapi3.SchemaRef, error) {
 }
 
 func (l *Latency) RegisterHandler(ctx context.Context, router *api.RoutingTree) {
-	router.Add(http.MethodGet, "/v1alpha1/latency", l.Handler)
+	router.Add(http.MethodGet, "v1alpha1/latency", l.Handler)
 }
 
 func (l *Latency) DeregisterHandler(ctx context.Context, router *api.RoutingTree) {
-	router.Remove(http.MethodGet, "/v1alpha1/latency")
+	router.Remove(http.MethodGet, "v1alpha1/latency")
 }
 
 func (l *Latency) Handler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 }
 
-func (l *Latency) check(ctx context.Context) (map[string]LatencyResultDTO, error) {
-	cl := http.Client{}
-	results := map[string]LatencyResultDTO{}
+func (l *Latency) check(ctx context.Context) (map[string]LatencyResult, error) {
+	log := logger.FromContext(ctx).WithGroup("check")
+	log.Debug("Checking latency")
+	var mu sync.Mutex
+	results := map[string]LatencyResult{}
 	wg, ctx := errgroup.WithContext(ctx)
 	// TODO mutex
 	for _, e := range l.cfg.Targets {
 		wg.Go(func(ctx context.Context, e string) func() error {
 			return func() error {
+				cl := http.Client{
+					Timeout: l.cfg.Timeout * time.Second,
+				}
 				req, err := http.NewRequestWithContext(ctx, http.MethodGet, e, nil)
 				if err != nil {
+					log.Error("Error while creating request", "error", err)
 					return err
 				}
-				var result LatencyResult
-				ctx = WithLatency(req.Context(), &result)
+
+				var latencyresult LatencyResult
 
 				req = req.WithContext(ctx)
 
+				start := time.Now()
 				response, err := cl.Do(req)
 				if err != nil {
 					errval := err.Error()
-					result.Error = &errval
+					latencyresult.Error = &errval
+					log.Error("Error while checking latency", "error", err)
+				} else {
+					latencyresult.Code = response.StatusCode
 				}
+				end := time.Now()
 
-				result.Code = response.StatusCode
-				// This does not need a mutex since the map key we're writing to is not dynamic
-				results[e] = result.ToDTO()
+				latencyresult.Total = end.Sub(start).Milliseconds()
+
+				mu.Lock()
+				defer mu.Unlock()
+				results[e] = latencyresult
 				return nil
 			}
 		}(ctx, e))
 	}
 
 	if err := wg.Wait(); err != nil {
+		log.Error("Error while checking latency", "error", err)
 		return nil, err
 	}
 
