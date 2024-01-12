0f55326ba346f18914fb6b5ce1d2f9ae852deea4
lvlcn-t <75443136+lvlcn-t@users.noreply.github.com>
2024-01-12 22:48:57 +0100

    docs: add decode godoc

--- internal/helper/decode.go
+++ internal/helper/decode.go
@@ -20,6 +20,42 @@ package helper
 
 import "github.com/mitchellh/mapstructure"
 
+// Decode is a generic function that takes an input of any type and attempts to decode it into a specified type T.
+// It leverages the mapstructure package for decoding the input data.
+//
+// Parameters:
+// - input: This is the source data that needs to be decoded. The input can be of any type, such as a map or a struct.
+//
+// Returns:
+//   - T: The function returns a value of type T, which is the target type that the input is decoded into.
+//     The generic type T allows this function to be flexible and used with various types.
+//   - error: If the decoding process encounters any issues, an error is returned. This could be due to a mismatch
+//     between the input and the target type T or other decoding issues.
+//
+// The function utilizes a DecoderConfig from the mapstructure package to set up the decoding process.
+// This configuration includes:
+//   - Setting WeaklyTypedInput to true, allowing for more flexible and forgiving decoding.
+//   - Custom DecodeHooks to convert string values to specific types like time.Duration or slice of strings,
+//     enhancing the function's ability to handle different types of input data.
+//
+// Example Usage:
+// Suppose we have a struct named MyConfig with fields A (int), B (string), and C ([]string).
+// We can use Decode to convert a map[string]any to MyConfig type as shown below:
+//
+//	configMap := map[string]any{
+//	    "A": "123",
+//	    "B": "example",
+//	    "C": "one,two,three",
+//	}
+//	var myConfig MyConfig
+//	myConfig, err := Decode[MyConfig](configMap)
+//	if err != nil {
+//	    // handle error
+//	}
+//
+// This function is particularly useful in settings where the type of the input data might vary,
+// such as when dealing with configuration files in different formats (JSON, YAML, etc.).
+// It provides a convenient and type-safe way to handle such dynamic data.
 func Decode[T any](input any) (T, error) {
 	var result T
 	config := &mapstructure.DecoderConfig{
