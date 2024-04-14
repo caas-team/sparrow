77079d19ca2aea665f2f958e83b964e5ca2fc7a5
lvlcn-t <75443136+lvlcn-t@users.noreply.github.com>
2024-04-14 22:46:22 +0200

    fix: add empty exporter to exporter registry

--- pkg/sparrow/metrics/exporter.go
+++ pkg/sparrow/metrics/exporter.go
@@ -1,3 +1,21 @@
+// sparrow
+// (C) 2024, Deutsche Telekom IT GmbH
+//
+// Deutsche Telekom IT GmbH and all other contributors /
+// copyright owners license this file to you under the Apache
+// License, Version 2.0 (the "License"); you may not use this
+// file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 package metrics
 
 import (
@@ -39,7 +57,7 @@ func (e Exporter) String() string {
 // Validate validates the protocol
 func (e Exporter) Validate() error {
 	switch e {
-	case HTTP, GRPC, STDOUT, NOOP:
+	case HTTP, GRPC, STDOUT, NOOP, "":
 		return nil
 	default:
 		return fmt.Errorf("unsupported exporter type: %s", e.String())
@@ -54,10 +72,10 @@ func (e Exporter) IsExporting() bool {
 // FileOpener is the function used to open a file
 type FileOpener func(string) (fs.File, error)
 
-// OpenFile is the function used to open a file
-var OpenFile FileOpener = func() FileOpener {
+// openFile is the function used to open a file
+var openFile FileOpener = func() FileOpener {
 	return func(name string) (fs.File, error) {
-		return os.Open(name)
+		return os.Open(name) // #nosec G304 // TODO: Can we hide this behind an interface instead?
 	}
 }()
 
@@ -67,7 +85,7 @@ func (e Exporter) TLSConfig(certFile string) (conf *tls.Config, err error) {
 		return nil, nil
 	}
 
-	file, err := OpenFile(certFile)
+	file, err := openFile(certFile)
 	if err != nil {
 		return nil, fmt.Errorf("failed to open certificate file: %w", err)
 	}
@@ -103,6 +121,7 @@ var registry = map[Exporter]exporterFactory{
 	GRPC:   newGRPCExporter,
 	STDOUT: newStdoutExporter,
 	NOOP:   newNoopExporter,
+	"":     newNoopExporter,
 }
 
 // Create creates a new exporter based on the configuration
