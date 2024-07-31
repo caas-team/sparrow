bdfac86af67694234b0f182722f11f7a004a2729
Niklas Treml <treml.niklas@gmail.com>
2024-07-31 11:09:48 +0200

    feat(ci): retry ci
    Signed-off-by: Niklas Treml <treml.niklas@gmail.com>

--- scripts/run_e2e_tests.sh
+++ scripts/run_e2e_tests.sh
@@ -3,14 +3,21 @@ export SPARROW_BIN=$(realpath ./dist/sparrow_linux_amd64_v1/sparrow)
 
 EXIT_CODE=0
 
+MAX_RETRY=3
+
 for i in $(ls e2e); do
-    echo "Running test e2e/$i"
-    cd e2e/$i
-    ./test.sh || {
-        EXIT_CODE=$?
-        echo "E2E test e2e/$i failed"
-    }
-    cd $root
+    for ATTEMPT in $(seq 1 $MAX_RETRY ); do
+        echo "[$ATTEMPT/$MAX_RETRY] Running test e2e/$i"
+        cd e2e/$i
+        ./test.sh 
+        TEST_EXIT_CODE=$?
+        cd $root
+        if [ $TEST_EXIT_CODE -eq 0 ]; then
+            break
+        elif [ $ATTEMPT -eq $MAX_RETRY ]; then
+            EXIT_CODE=1
+        fi
+    done 
 done
 
 exit $EXIT_CODE
