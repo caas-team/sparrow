68a9c4484b01d5c28972eab2d01a7291a2011f07
Bruno Bressi <puffito@gmail.com>
2024-12-25 10:23:56 +0100

    chore: shutdown message on end

--- pkg/sparrow/run_errors.go
+++ pkg/sparrow/run_errors.go
@@ -18,12 +18,15 @@
 
 package sparrow
 
+// ErrShutdown holds any errors that may
+// have occurred during shutdown of the Sparrow
 type ErrShutdown struct {
 	errAPI     error
 	errTarMan  error
 	errMetrics error
 }
 
+// HasError returns true if any of the errors are set
 func (e ErrShutdown) HasError() bool {
 	return e.errAPI != nil || e.errTarMan != nil || e.errMetrics != nil
 }
